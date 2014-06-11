2acae20c2382b8c61d4346175dea138b0fb59a3e
Jacinda Zhong <jacindaz@gmail.com>
2014-06-10 20:50:40 -0400

    Adds migration for meetups table in meetups database
     - creates meetups table
     - migrates to postgresql database

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140606200404) do
+ActiveRecord::Schema.define(version: 20140611003919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "meetups", force: true do |t|
+    t.string   "name",        null: false
+    t.string   "location",    null: false
+    t.text     "description", null: false
+    t.integer  "members_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "provider",   null: false
